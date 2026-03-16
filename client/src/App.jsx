d421280ade9400c3960598a1697459228938dce2
Enayat <enayatansari33@gmail.com>
2026-03-16 15:22:17 +0530

    Project Initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-lime-700 text-2xl text-center'>Hello From Client Side</h1>
+    </>
+  )
+}
+
+export default App
